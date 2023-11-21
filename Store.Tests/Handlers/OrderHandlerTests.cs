3f9e738bb257d3dbc953ca544d384511e9d4967c
lucascidade <lucascidade96@gmail.com>
2023-11-21 18:08:29 -0400

    estudo sobre testes de unidade e refatoração, nov

---
+++ Store.Tests/Handlers/OrderHandlerTests.cs
@@ -0,0 +1,120 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Store.Domain.Commands;
+using Store.Domain.Handlers;
+using Store.Domain.Repositories.Interfaces;
+using Store.Tests.Repositories;
+
+namespace Store.Tests.Handlers
+{
+    [TestClass]
+    public class OrderHandlerTests
+    {
+        private readonly ICustomerRepository _customerRepository;
+        private readonly IDeliveryFeeRepository _deliveryFeeRepository;
+        private readonly IDiscountRepository _discountRepository;
+        private readonly IOrderRepository _orderRepository;
+        private readonly IProductRepository _productRepository;
+
+        public OrderHandlerTests()
+        {
+            _customerRepository = new FakeCustomerRepository();
+            _deliveryFeeRepository = new FakeDeliveryFeeRepository();
+            _discountRepository = new FakeDiscountRepository();
+            _orderRepository = new FakeOrderRepository();
+            _productRepository = new FakeProductRepository();
+        }
+
+        [TestMethod]
+        [TestCategory("Handlers")]
+        public void Dado_um_cliente_inexistente_o_pedido_nao_deve_ser_gerado()
+        {
+            // TODO: Implementar
+            var customer = new CreateOrderCommand();
+            customer.Customer = "11111111";
+            customer.ZipCode = "12345678";
+            customer.PromoCode = "12345678";
+            customer.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            customer.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            customer.Validate();
+            Assert.AreEqual(customer.Valid, false);
+        }
+
+        [TestMethod]
+        [TestCategory("Handlers")]
+        public void Dado_um_cep_invalido_o_pedido_deve_ser_gerado_normalmente()
+        {
+            // TODO: Implementar
+            var command = new CreateOrderCommand();
+            command.Customer = "12345678911";
+            command.ZipCode = "0";
+            command.PromoCode = "12345678";
+            command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            command.Validate();
+
+            var handler = new OrderHandler(
+                _customerRepository,
+                _deliveryFeeRepository,
+                _discountRepository,
+                _productRepository,
+                _orderRepository);
+
+            handler.Handle(command);
+            Assert.AreEqual(handler.Valid, true);
+
+        }
+
+        [TestMethod]
+        [TestCategory("Handlers")]
+        public void Dado_um_promocode_inexistente_o_pedido_deve_ser_gerado_normalmente()
+        {
+            // TODO: Implementar
+
+        }
+
+        [TestMethod]
+        [TestCategory("Handlers")]
+        public void Dado_um_pedido_sem_itens_o_mesmo_nao_deve_ser_gerado()
+        {
+            // TODO: Implementar
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        [TestCategory("Handlers")]
+        public void Dado_um_comando_invalido_o_pedido_nao_deve_ser_gerado()
+        {
+            var command = new CreateOrderCommand();
+            command.Customer = "";
+            command.ZipCode = "13411080";
+            command.PromoCode = "12345678";
+            command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            command.Validate();
+
+            Assert.AreEqual(command.Valid, false);
+        }
+
+        [TestMethod]
+        [TestCategory("Handlers")]
+        public void Dado_um_comando_valido_o_pedido_deve_ser_gerado()
+        {
+            var command = new CreateOrderCommand();
+            command.Customer = "12345678";
+            command.ZipCode = "13411080";
+            command.PromoCode = "12345678";
+            command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+
+            var handler = new OrderHandler(
+                _customerRepository,
+                _deliveryFeeRepository,
+                _discountRepository,
+                _productRepository,
+                _orderRepository);
+
+            handler.Handle(command);
+            Assert.AreEqual(handler.Valid, true);
+        }
+    }
+}
